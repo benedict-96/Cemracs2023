869949eae7a4c30cb88d33d5560dce906a617eb3
benedict-96 <benedikt.brantner@icloud.com>
2023-07-18 16:33:07 +0200

    Fixed problem with printing the results.

--- transformer_scripts/transformer_comparison.jl
+++ transformer_scripts/transformer_comparison.jl
@@ -43,98 +43,92 @@ end
 train_y_encoded = encode_y(train_y)
 test_y_encoded = encode_y(test_y)
 
-function neural_network_setup_and_training(L=8, n_epochs=1)
-	
-   use_softmax=true
-   function relu(x::T) where T<:Real
-       max(zero(T), x)
-   end
-   activation=relu
-   #activation=tanh
- 
-   #named tuple of models that are compared
-   models = (
+function relu(x::T) where T<:Real
+    max(zero(T), x)
+end
+
+write_to_file = read("training_results.txt", String)
+function neural_network_setup_and_training(L=8, n_epochs=1, use_softmax=true, activation=relu, batch_size=128)
+
+    #named tuple of models that are compared
+    models = (
         model₀ = Lux.Chain(Tuple(map(_ -> ResNet(49, activation), 1:L))..., Classification(patch_length^2, 10, use_bias=false, use_average=false, use_softmax=use_softmax)),
         model₁ = Lux.Chain(Transformer(patch_length^2, n_heads, L, add_connection=true, Stiefel=false, activation=activation),
                         Classification(patch_length^2, 10, use_bias=false, use_average=false, use_softmax=use_softmax)),
         model₂ = Lux.Chain(Transformer(patch_length^2, n_heads, L, add_connection=true, Stiefel=true, activation=activation),
                      Classification(patch_length^2, 10, use_bias=false, use_average=false, use_softmax=use_softmax))
- )
+    )
 
-#write_to_file = ""
-write_to_file = read("training_results.txt", String) 
 
-function training(model::Lux.Chain, batch_size=32, n_epochs=.01, o=AdamOptimizer(), enable_cuda=false, give_training_error=false)
-    o.t = 0
-    ps, st = enable_cuda ? Lux.setup(CUDA.device(), Random.default_rng(), model) : Lux.setup(Random.default_rng(), model)
+    function training(model::Lux.Chain, batch_size=32, n_epochs=.01, o=AdamOptimizer(), enable_cuda=false, give_training_error=false)
+        o.t = 0
+        ps, st = enable_cuda ? Lux.setup(CUDA.device(), Random.default_rng(), model) : Lux.setup(Random.default_rng(), model)
+
+        function loss(ps, x, y)
+            x_eval = enable_cuda ? Lux.apply(model, x |> cu, ps, st)[1] : Lux.apply(model, x, ps, st)[1]
+            enable_cuda ? norm(x_eval - (y |> cu))/sqrt(size(y, 2)) : norm(x_eval - (y))/sqrt(size(y, 2))
+        end
                     
-    function loss(ps, x, y)
-        x_eval = enable_cuda ? Lux.apply(model, x |> cu, ps, st)[1] : Lux.apply(model, x, ps, st)[1]
-        enable_cuda ? norm(x_eval - (y |> cu))/sqrt(size(y, 2)) : norm(x_eval - (y))/sqrt(size(y, 2))
-    end
-                    
-    # the number of training steps is calculated based on the number of epochs and the batch size
-    training_steps = Int(ceil(n_epochs*num/batch_size))
-    # this records the training error
-    loss_array = zeros(training_steps + 1)
-    loss_array[1] = give_training_error ? (enable_cuda ? loss(ps, train_x_reshaped |> cu, train_y_encoded |> cu) : loss(ps, train_x_reshaped, train_y_encoded)) : 0.
-                    
-    give_training_error ? println("initial loss: ", loss_array[1]) : nothing
-                    
-    # initialize the optimizer cache
-    optimizer_instance = enable_cuda ? Optimizer(CUDA.device(), o, model) : Optimizer(o, model)
-                    
-    @showprogress "Training network ..." for i in 1:training_steps
-        # draw a mini batch 
-        indices = Int.(ceil.(rand(batch_size)*num))
-        x_batch = enable_cuda ? (train_x_reshaped[:, :, indices] |> cu) : train_x_reshaped[:, :, indices]
-        y_batch = enable_cuda ? (train_y_encoded[:, indices] |> cu) : train_y_encoded[:, indices]
-                    
-        # compute the gradient using Zygote
-        dp = gradient(ps -> loss(ps, x_batch, y_batch), ps)[1]
-                    
-        #update the cache of the optimizer and the parameter
-        optimization_step!(optimizer_instance, model, ps, dp)    
-                    
-        # compute the loss at the current step
-        loss_array[1+i] = give_training_error ? (enable_cuda ? loss(ps, train_x_reshaped |> cu, train_y_encoded |> cu) : loss(ps, train_x_reshaped, train_y_encoded)) : 0.
-                    
-    end
-    #println("final loss: ", loss_array[end])
-    test_loss = enable_cuda ? loss(ps, test_x_reshaped |> cu, test_y_encoded |> cu) : loss(ps, test_x_reshaped, test_y_encoded)
-    println("final test loss: ", test_loss, "\n")
+        # the number of training steps is calculated based on the number of epochs and the batch size
+        training_steps = Int(ceil(n_epochs*num/batch_size))
+        # this records the training error
+        loss_array = zeros(training_steps + 1)
+        loss_array[1] = give_training_error ? (enable_cuda ? loss(ps, train_x_reshaped |> cu, train_y_encoded |> cu) : loss(ps, train_x_reshaped, train_y_encoded)) : 0.
+
+        give_training_error ? println("initial loss: ", loss_array[1]) : nothing
+
+        # initialize the optimizer cache
+        optimizer_instance = enable_cuda ? Optimizer(CUDA.device(), o, model) : Optimizer(o, model)
+
+        @showprogress "Training network ..." for i in 1:training_steps
+            # draw a mini batch 
+            indices = Int.(ceil.(rand(batch_size)*num))
+            x_batch = enable_cuda ? (train_x_reshaped[:, :, indices] |> cu) : train_x_reshaped[:, :, indices]
+            y_batch = enable_cuda ? (train_y_encoded[:, indices] |> cu) : train_y_encoded[:, indices]
+
+            # compute the gradient using Zygote
+            dp = gradient(ps -> loss(ps, x_batch, y_batch), ps)[1]
+
+            #update the cache of the optimizer and the parameter
+            optimization_step!(optimizer_instance, model, ps, dp)    
+
+            # compute the loss at the current step
+            loss_array[1+i] = give_training_error ? (enable_cuda ? loss(ps, train_x_reshaped |> cu, train_y_encoded |> cu) : loss(ps, train_x_reshaped, train_y_encoded)) : 0.
+        end
+        #println("final loss: ", loss_array[end])
+        test_loss = enable_cuda ? loss(ps, test_x_reshaped |> cu, test_y_encoded |> cu) : loss(ps, test_x_reshaped, test_y_encoded)
+        println("final test loss: ", test_loss, "\n")
     
-    global write_to_file *= "L = "*string(L)*", n_epochs = "*string(n_epochs)*", batch_size = "*string(batch_size)*", Optimizer = "*string(o)*", test_loss = "*string(test_loss)*"\n"
-    (loss_array=loss_array, ps=ps)
-end
+        global write_to_file *= "L = "*string(L)*", n_epochs = "*string(n_epochs)*", batch_size = "*string(batch_size)*", Optimizer = "*string(o)*", test_loss = "*string(test_loss)*"\n"
+        (loss_array=loss_array, ps=ps)
+    end
+    
+    o = AdamOptimizer(.001f0, 0.9f0, 0.99f0, 1.0f-8)
+    enable_cuda = true
+    give_training_error = false
 
-batch_size = 128
-o = AdamOptimizer(.001f0, 0.9f0, 0.99f0, 1.0f-8)
-enable_cuda = true
-give_training_error = false
+    loss_arrays = NamedTuple{keys(models)}(Tuple(training(model, batch_size, n_epochs, o, enable_cuda, give_training_error) for model in models))
 
-loss_arrays = NamedTuple{keys(models)}(Tuple(training(model, batch_size, n_epochs, o, enable_cuda, give_training_error) for model in models))
+    global write_to_file *= "\n"
 
-write_to_file *= "\n"
-open("training_results.txt", "w") do file
-	write(file, write_to_file)
-end
+    function plot_stuff()
+        p = plot() 
+        [plot!(p, loss_array[key], label=string(key)) for key in keys(loss_array)]
+        p
+    end
 
-function plot_stuff()
-    p = plot() 
-    [plot!(p, loss_array[key], label=string(key)) for key in keys(loss_array)]
-    p
-end
-
-p = give_training_error ? plot_stuff : nothing 
-display(p)
+    p = give_training_error ? plot_stuff : nothing 
+    display(p)
 
 end
 
-L_choices = (2,4,8,16)
-n_epochs_choices = (2,4,8,16,32)
+L_choices = (2,4)
+n_epochs_choices = (2,4)
 for L in L_choices
 	for n_epochs in n_epochs_choices
 		neural_network_setup_and_training(L, n_epochs)
 	end
+end
+open("training_results.txt", "w") do file
+    write(file, write_to_file)
 end
